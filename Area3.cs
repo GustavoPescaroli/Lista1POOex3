d28dc32f997eb5f21fa51dc98ceedc09c63d8150
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:34:12 -0300

    Add files via upload

---
+++ Area3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Diagonal t;
+            t = new Diagonal();
+
+
+            Console.WriteLine("Digite o valor da diagonal");
+            t.setd(double.Parse(Console.ReadLine()));
+            t.calcular();
+            Console.WriteLine("O valor da area {0}", t.geta());
+
+
+
+
+        }
+    }
+}
